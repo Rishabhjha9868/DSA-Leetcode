9301f305dc7f4a74b39356db788ba6456bf32beb
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-02-02 14:23:55 +0530

    Time: 19 ms (14.04%), Space: 8.1 MB (24.41%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int m =x;
+        long reverse = 0;
+        while(m!=0)
+        {
+            reverse = reverse*10 +m%10;
+            m = m/10;
+        }
+        if(x<0)
+        {
+            return false;
+        }
+       else if(reverse==x)
+        {
+            return true;
+        }
+        return false;
+    }
+};
