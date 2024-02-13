58aa57e0509eb0aed7fa0dc7e82efaba38199329
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-02-13 19:30:35 +0530

    Time: 28 ms (61.30%), Space: 30 MB (10.25%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,8 +5,10 @@ public:
         int s = 0, e = n;
         while( s <= e){
             int mid = s + (e-s)/2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] > target) e = mid -1;
+            if (nums[mid] == target) 
+                return mid;
+            else if (nums[mid] > target)
+                e = mid -1;
             else s = mid + 1;
         }
         return -1;
