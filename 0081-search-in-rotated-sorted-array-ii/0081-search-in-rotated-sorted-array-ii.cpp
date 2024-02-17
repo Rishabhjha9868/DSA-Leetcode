00ca7a47c3dcbfbc25cc78c0dbd964709a1e16ee
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-02-18 00:02:40 +0530

    Time: 11 ms (8.22%), Space: 17.1 MB (14.21%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -5,10 +5,7 @@ public:
         for(int i=0; i<nums.size(); i++)
         {
           if(nums[i]==target)
-          {
-            return 1;
-          }
-            
+            return 1;   
         }return 0;
     }
 };
