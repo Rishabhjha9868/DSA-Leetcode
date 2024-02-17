3536059a5b086a8ea002f8cdd5c6dc6783d56400
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-02-17 13:06:06 +0530

    Time: 3 ms (91.43%), Space: 15.9 MB (43.02%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,43 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // Initialize variables to store the first and last index of the target
-        int fidx = -1;
-        int lidx = -1;
+     
+        int first = -1;
+        int last = -1;
         
-        // Binary search to find the first occurrence of the target
-        int l = 0, r = nums.size() - 1;
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
+        // Lower Bound
+        int s = 0, e = nums.size() - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
             if (nums[mid] == target) {
-                // Update first index and continue searching in the left half
-                fidx = mid;
-                r = mid - 1;
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+                first = mid;
+                e = mid - 1;
+            } 
+            else if (nums[mid] > target) {
+                e = mid - 1;
+            } 
+            else {
+                s = mid + 1;
+            }
+        }
+       // Upper Bound;
+        s = 0;
+        e = nums.size() - 1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target) { 
+                last = mid;
+                s = mid + 1;
+            } 
+            else if (nums[mid] > target) {
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
             }
         }
 
-        // Reset the search boundaries
-        l = 0;
-        r = nums.size() - 1;
-
-        // Binary search to find the last occurrence of the target
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] == target) {
-                // Update last index and continue searching in the right half
-                lidx = mid;
-                l = mid + 1;
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-
-        return {fidx, lidx};
+        return {first, last};
     }
 };
