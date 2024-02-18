eeec054caa5bb751c890b900b46e2d3a3446613e
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-02-18 15:31:23 +0530

    Time: 18 ms (72.45%), Space: 24.7 MB (9.43%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -12,10 +12,11 @@ public:
             if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
                 return nums[mid]; 
             }
-            if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) 
-            || (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+            if((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || (mid % 2 == 0 && nums[mid] == nums[mid + 1]))
+            {
                 low = mid + 1;
-            } else {
+            }
+            else {
                 high = mid - 1;
             }
         }
