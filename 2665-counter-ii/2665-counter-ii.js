1df17d55f1e6b01c43a5679672b1e73f53fa3285
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-03-07 15:18:24 +0530

    Time: 61 ms (45.22%), Space: 51.7 MB (41.10%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    var value = init;
+    function increment(){
+        return value +=1;
+    }
+    function decrement(){
+        return value-=1;
+    }
+    function reset(){
+        return value = init;
+    }
+    return {increment, decrement, reset}
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
