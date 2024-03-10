f3015eede02816428e042ec31253aeb382113160
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-03-10 10:20:35 +0530

    Time: 10 ms (20.45%), Space: 12.7 MB (46.54%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,9 +4,9 @@ public:
         
         vector<int>ans;
         set<int>s;
-        // int m = nums2.size(), n = nums1.size();
-        for(int i=0; i<nums1.size(); i++){
-            for(int j =0;j<nums2.size(); j++){
+        int m = nums2.size(), n = nums1.size();
+        for(int i=0; i<n; i++){
+            for(int j =0;j<m; j++){
                 if(nums1[i]==nums2[j]){
                     s.insert(nums1[i]);
                     break;
