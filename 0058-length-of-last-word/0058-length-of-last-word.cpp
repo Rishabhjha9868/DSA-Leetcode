9748d01582753d68c7be4d299f1b65499fe195ca
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-04-01 14:53:13 +0530

    Time: 2 ms (54.22%), Space: 7.6 MB (87.89%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,7 +3,6 @@ public:
     int lengthOfLastWord(string s) {
         int length = 0;
         bool counting = false;
-        
         for (int i = s.length() - 1; i >= 0; i--) {
             if (s[i] != ' ') {
                 counting = true;
