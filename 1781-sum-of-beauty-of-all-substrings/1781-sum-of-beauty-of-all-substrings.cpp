bc5370d2ce73fc350d4f78a3af2170d88f156ad2
Rishabh Jha <147179883+Rishabhjha9868@users.noreply.github.com>
2024-04-03 15:19:39 +0530

    Time: 95 ms (83.48%), Space: 9.7 MB (85.11%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minimum(vector<int> &mp){
+        int k = INT_MAX;
+        for(auto it : mp){
+            if(it<=k && it>0)
+                k = it;
+        }
+        return k;
+    }
+    int maximum(vector<int> &mp){
+        int k = INT_MIN;
+        for(auto it : mp){
+            if(it>=k)
+                k = it;
+        }
+        return k;
+    }
+    int beautySum(string s) {
+        int sum = 0;
+        for(int i = 0; i<s.size();i++){
+            vector<int> mp(26,0);
+            for(int j=i; j<s.size();j++){
+                mp[s[j]-'a']++;
+                int mx = maximum(mp);
+                int mn = minimum(mp);
+                sum += mx - mn;
+            }
+        }
+        return sum;
+    }
+};
